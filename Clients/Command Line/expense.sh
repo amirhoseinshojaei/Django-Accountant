dd39992bf2c3d550839581322ecd56dd8ecf3677
amirhosein <amirhoseinbu80@gmail.com>
2023-11-25 21:50:07 +0330

    add income.sh and expense.sh

---
+++ Clients/Command Line/expense.sh
@@ -0,0 +1,7 @@
+#bin/bash
+# please set variables
+token = # set the token
+
+curl --data "token  = $1 , bought = $2 , amount = $3 , date = $4" http:localhost:8000/submit/expense/
+
+# If you don't enter the date, the date will automatically go to now
