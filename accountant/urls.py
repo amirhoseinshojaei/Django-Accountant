e7fc535aeca867bda1e3e9af975769ee39dd5da1
amirhosein <amirhoseinbu80@gmail.com>
2023-11-25 16:32:28 +0330

    urls for index

--- accountant/urls.py
+++ accountant/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns=[
     path ('submit/expense/',views.submit_expense, name= 'sumbit_expense'),
     path ("submit/income/",views.submit_income, name= 'submit_income'),
+    path ('',views.index, name= 'index'),
 ]
