da37ede7edcacbc243af93f19571d3903f820d4a
amirhosein <amirhoseinbu80@gmail.com>
2023-11-27 00:45:19 +0330

    edit income.sh & expense.sh

--- Clients/Command Line/expense.sh
+++ Clients/Command Line/expense.sh
@@ -1,6 +1,5 @@
 #bin/bash
-# please set variables
-token = # set the token
+
 
 curl --data "token  = $1 , bought = $2 , amount = $3 , date = $4" http:localhost:8000/submit/expense/
 
--- Clients/Command Line/income.sh
+++ Clients/Command Line/income.sh
@@ -1,7 +1,6 @@
 #bin/bash
 # please set variables
-token = # set the token
 
-curl --data "token  = $1 , text = $2 , amount = $3 , date = $4" http:localhost:8000/submit/income/
+curl --data "token  = $1 , text = $2 , amount = $3 , date = $4" http://localhost:8000/submit/income
 
 # If you don't enter the date, the date will automatically go to now
