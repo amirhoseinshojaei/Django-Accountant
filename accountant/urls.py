3917045c89282f8d3f33134d6c84a7f161d377fa
amirhosein <amirhoseinbu80@gmail.com>
2023-11-27 00:44:08 +0330

    url general view

--- accountant/urls.py
+++ accountant/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns=[
     path ('submit/expense/',views.submit_expense, name= 'sumbit_expense'),
     path ("submit/income/",views.submit_income, name= 'submit_income'),
+    path ("query/generalstat/",views.generalstat, name= 'generalstat'),
     path ('',views.index, name= 'index'),
 ]
