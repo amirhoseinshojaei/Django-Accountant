678e8b978a9e8c62a4b0ba85929e7f0b840abc4f
amirhosein <amirhoseinbu80@gmail.com>
2023-12-03 22:50:45 +0330

    create incomestat

--- accountant/views.py
+++ accountant/views.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from .models import (Expense,Income)
 from django.views.decorators.csrf import csrf_exempt
 from django.db.models import Aggregate , Count , Sum
+from django.contrib.auth.hashers import check_password
 # Create your views here.
 
 @csrf_exempt
@@ -116,6 +117,35 @@ def generalstat(request):
         return JsonResponse({
             'error':'invalid request'
         }, status = 400)
+    
+def income_stat(request):
+    if request.POST.__contains__('username') and  request.POST.__contains__('password'):
+        username = request.POST.get ('username')
+        password = request.POST.get ('password')
+        try:
+            this_user = User.objects.get(username = username)
+        except User.DoesNotExist:
+            context:{}
+            context['error'] = 'invalid user'
+            return JsonResponse (
+                context,
+                encoder= JSONEncoder
+            )
+        if (check_password(password, this_user.password)):
+            income = Income.objects.get(user = this_user)
+            context:{}
+            context["income"] = income
+            return JsonResponse(context , encoder= JSONEncoder)
+        else:
+            return JsonResponse ({
+                'error': "your password is wrong"
+            }, status = 400)
+        
+    else:
+        return JsonResponse({
+            "error": 'your request method is not safe'
+        }, status = 400)
+# TODO:create expensestat
     
 
 
