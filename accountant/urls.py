d773a9dbd93c898eb8bd9d18dafe907e6ad06f58
amirhosein <amirhoseinbu80@gmail.com>
2023-12-06 23:35:24 +0330

    url for query_income

--- accountant/urls.py
+++ accountant/urls.py
@@ -7,5 +7,6 @@ urlpatterns=[
     path ("query/generalstat/",views.generalstat, name= 'generalstat'),
     path ('incomestat/',views.income_stat, name = 'incomestat'),
     path ('expensestat/', views.expense_stat , name = 'expensestat'),
+    path ('query/incomes/', views.query_income, name = 'query_incomes'),
     path ('',views.index, name= 'index'),
 ]
