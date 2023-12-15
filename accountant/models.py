6379d30263f5ebfcb0daff7054baabc0af6a0599
amirhosein <amirhoseinbu80@gmail.com>
2023-12-15 22:23:35 +0330

    add str to new models

--- accountant/models.py
+++ accountant/models.py
@@ -35,3 +35,6 @@ class New(models.Model):
     description = models.TextField (max_length = 1500)
     date = models.DateTimeField (auto_now=False,auto_now_add=True)
     user = models.ForeignKey (User , on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.title
