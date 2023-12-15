01579a6f5aaa00c4c4f28874419fbec1790d5610
amirhosein <amirhoseinbu80@gmail.com>
2023-12-15 21:40:38 +0330

    models for news

--- accountant/models.py
+++ accountant/models.py
@@ -29,3 +29,9 @@ class Income(models.Model):
 
     def __str__(self):
         return "{}-{}".format(self.date,self.text)
+    
+class New(models.Model):
+    title = models.CharField (max_length = 255)
+    description = models.TextField (max_length = 1500)
+    date = models.DateTimeField (auto_now=False,auto_now_add=True)
+    user = models.ForeignKey (User , on_delete = models.CASCADE)
